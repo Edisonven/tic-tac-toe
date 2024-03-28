8a311507125731f3df440ff2fc6e1fff448a5383
Edison <aalevenegass@gmail.com>
2024-03-28 12:41:21 -0300

    Creando variable con turnos y estado para manejar el tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const turns = {
+    X: "x",
+    O: "o",
+  };
+
+  const [board, setBoard] = useState(array(9).fill(null));
+
   return (
     <div className="app__container">
       <h1>Hola soy la app</h1>
