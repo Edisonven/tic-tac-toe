ba37cf979b01875b2f4cf4d996eb6e61a5626be5
Edison <aalevenegass@gmail.com>
2024-03-28 13:02:45 -0300

    Mapeando arreglo para tablero

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,22 @@ function App() {
     O: "o",
   };
 
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(Array(9).fill("hola"));
 
   return (
     <div className="app__container">
-      <h1 className="app__title">Tic - Tac - Toe</h1>
+      <main className="app__board">
+        <h1 className="app__title">Tic - Tac - Toe</h1>
+        <section className="app__game">
+          {board.map((board) => {
+            return (
+              <div className="app__game__cell">
+                <span className="app__game__cellcontent">{board}</span>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </div>
   );
 }
