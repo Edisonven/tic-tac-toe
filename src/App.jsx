be6cb378f421729c8f4d3301d54122a34c23423d
Edison <aalevenegass@gmail.com>
2024-03-28 14:35:49 -0300

    Creando arreglo con combinaciones ganadoras

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,20 @@ function App() {
     O: "o",
   };
 
+  const winnerComibatios = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(turns.X);
+  const [winner, setWinner] = useState("");
 
   const updateBoard = (index) => {
     if (board[index]) {
